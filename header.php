42227288fdcc1748c569f7747f8916b844656697
jookllo <hokello34@gmail.com>
2019-05-23 13:12:27 +0300

    Creation of header and footer css and div classes

---
+++ header.php
@@ -0,0 +1,14 @@
+<!DOCTYPE>
+<html>
+    <header>
+        <title>Home</title>
+        <link rel="stylesheet" type="text/css" href="lib/css/mystyle.css"> 
+    </header>
+    <body>
+        <div class="nav">
+            <a href="home.php">Home</a>
+            <a href="menu.php">Menu</a>
+            <a href="blog.php">Blog</a>
+        </div>
+    
+  
