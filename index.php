243b570e2e0c53534c03521fcd4f560923c87551
jookllo <hokello34@gmail.com>
2019-05-23 20:07:19 +0300

    Addition to the navbar and also edits on the footer

--- index.php
+++ index.php
@@ -1,7 +1,40 @@
-<?php
-    
-    include_once"header.php";
+<!DOCTYPE>
+<html>
+    <header>
+        <title>Home</title>
+        <link rel="stylesheet" type="text/css" href="lib/css/mystyle.css"> 
+    </header>
+    <body>
+        <div class="nav">
+            <a class="top" href="index.php">Home
+                <div class="underline"></div>
+            </a>
+            <a class="top" href="menu.php">Menu
+                <div class="underline"></div>
+            </a>
+            <a class="top" href="blog.php">Blog
+                <div class="underline"></div>
+            </a>
+        </div>
+        </body>
+    <div class="footer">
+        <div class="side-footer1">
+            <img src="" alt=""/>
+        </div>
+        <div class="side-footer2">
+            <a class="link" href="index.php">Home</a><br/><br/>
+            <a class="link" href="menu.php">Menu</a><br/><br/>
+            <a class="link" href="blog.php">Blog</a><br/><br/>
+            <a class="link" href="news.php">News</a>
+        </div>
+        <div class="side-footer3">
+            <h3>Contact Us At:</h3>
+                <a class="link">+254 711111111</a><br/>
+                <a class="link" href="info@google.com">Email</a><br/>
+                <a class="link" href="#">Facebook</a><br/>
+                <a class="link" href="#">Twitter</a><br/>
+                <a class="link" href="#">Google+</a><br/>
 
-
-    include_once"footer.php";
-?>
+        </div>
+    </div>
+</html>
