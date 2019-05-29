e4f240bcfae4beea90a22c27d9151c3363ef79ae
jookllo <hokello34@gmail.com>
2019-05-29 10:15:15 +0300

    Artists details have been added to the bands part

--- bands.php
+++ bands.php
@@ -17,7 +17,23 @@
        			<a href="bands.php">Band</a>
        		</li>
        </ul>
-
+        <h2>Band Play Saturdays</h2>
+        <p>The restaurant will sometimes call various bands to come play wiothin the establishment. This is usually done in the balcony of the restaurant.<br> Some artists perform with live bands in the afternoon which also allows for an interesting Sunday lunch with the family on behalf of us.</p>
+        <h3>Some of the frequent musicians include :</h3>
+        <li>
+          <ul>Blinky Bill</ul>
+          <img src=""/>
+          <p>Man of various genres. Brings the vide to the building and make the party much more interesting. He makes a variety of music for the audience to enjoy and also has an interesting tone. </p>
+          <ul>Nyashinski</ul>
+          <img src=""/>
+          <p>He recently joined the usual musicians to play on sundays but does so on his own timing. The artist always brings the party with him and he makes the mood quite interesting. </p>
+          <ul>Just A Band</ul>
+          <img src=""/>
+          <p>With their on stage antics, they are a not a mainstream band but they understand how to get a party started. This group is talented and the unlimited talent they posess makes it interesting to see how they fair off.</p>
+        </li>
+        <h3>Let us hear who you'd like us to call in to present in our Band Play Sunday's.</h3>
+        <input type="text" name="band">
+        <button type="button">Submit</button>
     </body>
     <footer>
       <strong>Westlands Opposite Java</strong>
