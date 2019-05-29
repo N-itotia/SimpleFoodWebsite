cd93c74db88568fe30f52fbd30aaf638ffdfc95a
jookllo <hokello34@gmail.com>
2019-05-30 01:53:10 +0300

    footer template

---
+++ footer.php
@@ -0,0 +1,7 @@
+<footer>
+    	<strong>Westlands Opposite Java</strong>
+    	<p>P.O. BOX 12344</p>
+    	<a href="info@gmail.com">info@mamamachakula.com</a>
+    	<p>+254712342231</p>
+    </footer>
+</html>
