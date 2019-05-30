ab89e2a633d3320ad5454990f73d64d5d7fbed33
Nigel <nigelnjagi@gmail.com>
2019-05-30 16:04:01 +0300

    ok

--- footer.php
+++ footer.php
@@ -1,7 +1,15 @@
-<footer>
-    	<strong>Westlands Opposite Java</strong>
-    	<p>P.O. BOX 12344</p>
-    	<a href="info@gmail.com">info@mamamachakula.com</a>
-    	<p>+254712342231</p>
-    </footer>
+<!-- Footer -->
+<footer class="py-5 bg-dark">
+  <div class="container">
+    <p class="m-0 text-center text-white">Copyright &copy; Your Website 2019</p>
+  </div>
+  <!-- /.container -->
+</footer>
+
+<!-- Bootstrap core JavaScript -->
+<script src="vendor/jquery/jquery.min.js"></script>
+<script src="vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+
+</body>
+
 </html>
--- index.php
+++ index.php
@@ -1,23 +1,71 @@
-		<title>MamaMachakula - Home</title>
-	</head>
-    <body>
-    	<h2>MamaMachakula</h2>
-       <ul>
-       		<li class="active">
-       			<a href="index.php">Home</a>
-       		</li>
-       		<li>
-       			<a href="menu.php">Menu</a>
-       		</li>
-       		<li>
-       			<a href="bands.php">Band</a>
-       		</li>
-       </ul>
-       <h3>Welcome to the simple dishes that remind you of home.</h3>
-       <p>Here you can get the simple joys of food that is made with a home made touch. We have a dedicated staff that fully focuses on the delicacies that make it worth while. Our menu changes everyday allowing for something new. This allows for fresh tasting food for the consumer to choose from. </p>
-       <p>We also have Bring a Band Saturdays where you can suggest a band to bring or we find local talent to play while you enjoy the environment while eating.</p>
-       <h3>Founded in 2007</h3>
-       <p>Our founder focused on the niche market that no other restaurant was looking at.<br><b>A relaxing envrionment with good looking food and satisfying food for the family.</b></p>
-       <p>This view allowed us to engage with customers and also improve quality service of the business.<br> Through constant feedback we are able to improve the service our restaurant offers throughout Nairobi.</p>
-    </body>
+<!DOCTYPE html>
+<html lang="en">
+
+<<?php include "header.php" ?>
+
+<body>
+
+
+
+  <!-- Page Content -->
+  <div class="container">
+
+    <!-- Heading Row -->
+    <div class="row align-items-center my-5">
+      <div class="col-lg-7">
+        <img class="img-fluid rounded mb-4 mb-lg-0" src="images.jpg" alt="">
+      </div>
+      <!-- /.col-lg-8 -->
+      <div class="col-lg-5">
+        <h1 class="font-weight-light">MamaMachakula</h1>
+        <p>Welcome to the simple dishes that remind you of home.
+       Here you can get the simple joys of food that is made with a home made touch. We have a dedicated staff that fully focuses on the delicacies that make it worth while. Our menu changes everyday allowing for something new. This allows for fresh tasting food for the consumer to choose from.</p>
+        <a class="btn btn-primary" href="menu.php">Menu</a>
+      </div>
+      <!-- /.col-md-4 -->
+    </div>
+    <!-- /.row -->
+
+    <!-- Call to Action Well -->
+    <div class="card text-white bg-secondary my-5 py-4 text-center">
+      <div class="card-body">
+        <p class="text-white m-0">We also have Bring a Band Saturdays where you can suggest a band to bring or we find local talent to play while you enjoy the environment while eating.</p>
+      </div>
+    </div>
+
+    <!-- Content Row -->
+    <div class="row">
+      <div class="col-md-4 mb-5">
+        <div class="card h-100">
+          <div class="card-body">
+            <h2 class="card-title">Founded in 2007</h2>
+            <p class="card-text">Our founder focused on the niche market that no other restaurant was looking at.<br><b>A relaxing envrionment with good looking food and satisfying food for the family.</b></p>
+          </div>
+          <div class="card-footer">
+            <a href="#" class="btn btn-primary btn-sm">More Info</a>
+          </div>
+        </div>
+      </div>
+      <!-- /.col-md-4 -->
+      <div class="col-md-4 mb-5">
+        <div class="card h-100">
+          <div class="card-body">
+          This view allowed us to engage with customers and also improve quality service of the business.<br> Through constant feedback we are able to improve the service our restaurant offers throughout Nairobi.</p>
+          </div>
+          <div class="card-footer">
+            <a href="#" class="btn btn-primary btn-sm">More Info</a>
+          </div>
+        </div>
+      </div>
+      <!-- /.col-md-4 -->
     
+      <!-- /.col-md-4 -->
+
+    </div>
+    <!-- /.row -->
+
+  </div>
+  <!-- /.container -->
+
+  <!-- Footer -->
+  <<?php include "footer.php" ?>
